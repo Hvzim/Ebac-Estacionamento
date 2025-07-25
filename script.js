7ec9e0d4bf5b259de7d72405975db7d155b03744
Gabriel Zatz Mazin <gabrielmazin7@gmail.com>
2025-07-25 12:04:41 -0300

    Corrigido Parquimetro

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-class parquimetro1{
-    constructor(pagamento){
-    // Propriedades
+class parquimetro1 {
+    constructor(pagamento) {
+        // Propriedades
         this.tempo = 0;
         this.pagamento = pagamento;
         this.troco = 0;
     }
     //Condição
-    valorMenorQueUm(){
-    if(this.pagamento < 1){
-        return "Valor Insuficiente, Insira pelo menos R$ 1,00."
-    }
+    valorMenorQueUm() {
+        if (this.pagamento < 1) {
+            return "Valor Insuficiente, Insira pelo menos R$ 1,00."
+        }
     }
     // Métodos
-    calcularTempo(){
-        if(this.pagamento >= 3){
+    calcularTempo() {
+        if (this.pagamento >= 3) {
             this.tempo = 120;
             this.troco = this.pagamento - 3;
-        } else if (this.pagamento >= 1.75 < 3 ){
+        } else if (this.pagamento >= 1.75 && this.pagamento < 3) {
             this.tempo = 60;
             this.troco = this.pagamento - 1.75;
-        }else if (this.pagamento >= 1.00 < 1.75){
+        } else if (this.pagamento >= 1.00 && this.pagamento < 1.75) {
             this.tempo = 30;
             this.troco = this.pagamento - 1.00;
+        } else {
+            return this.valorMenorQueUm(); // trata valor insuficiente
         }
         return `Tempo: ${this.tempo} minutos. Troco: R$${this.troco.toFixed(2)}`;
     }
 
+
 }
-function calcularTempo(){
+function calcularTempo() {
     const valor = parseFloat(document.getElementById("valor").value);
     const parq = new parquimetro1(valor);
     const resultado = parq.calcularTempo();
